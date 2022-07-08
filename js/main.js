b185ee44b7612fa610e6b37b5ce7f886b7892479
Andrea Quaglioni <quaglioniandrea@gmail.com>
2022-07-08 14:43:25 +0200

    add: added .card objects through JS

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-* {
+const images = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg"];
 
+let activeSlide = 0;
+
+const sliderDom = document.querySelector('.slider');
+
+for (let i = 0; i < images.length; i++) {
+    sliderDom.innerHTML += `<div class="card"><img class="card-img" src="${images[i]}"></div>`
 }
+
