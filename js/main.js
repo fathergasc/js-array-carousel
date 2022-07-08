8ec5cc191e86eefc458eef274f35a9b8ee34f362
Andrea Quaglioni <quaglioniandrea@gmail.com>
2022-07-08 15:08:43 +0200

    add: selected all card objects, added class .show to first .card objects, all others are d-none, set const for .next & .previous to setup eventListener

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const images = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg"];
 
-let activeSlide = 0;
+let firstImage = 0;
 
 const sliderDom = document.querySelector('.slider');
 
@@ -8,3 +8,8 @@ for (let i = 0; i < images.length; i++) {
     sliderDom.innerHTML += `<div class="card"><img class="card-img" src="${images[i]}"></div>`
 }
 
+const imagesList = document.querySelectorAll('.card');
+imagesList[firstImage].classList.add('show');
+
+const nextDom = document.getElementsByClassName('next')
+const previousDom = document.getElementsByClassName('previous')
